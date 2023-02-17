ebb068a72354d23b1984f3f86c14fbe83176471f
Skytanic <skytanictv@gmail.com>
2023-02-16 18:58:19 -0800

    Update mod information and versions

---
+++ src/main/java/net/skytanic/ignotus/IgnotusClient.java
@@ -0,0 +1,11 @@
+package net.skytanic.ignotus;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class IgnotusClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient()
+	{
+
+	}
+}
